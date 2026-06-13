98ccfd6e3b12f4abaca42be2bff6ed130dc5bb04
rajuyadav-cs <rajuyadav242625@gmail.com>
2026-06-13 19:59:09 +0530

    template-form-views done

--- Phase 2 - Django Core/2.4 Views Templates Forms/djangomodels/genshin/urls.py
+++ Phase 2 - Django Core/2.4 Views Templates Forms/djangomodels/genshin/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('/charform', views.charform, name = 'charform')
+    path('',views.home, name = 'home'),
+    path('listpage', views.listpage, name='listpage'),
+    path('signup', views.signup, name = 'signup'),
+    path('login', views.login_view, name = 'login'),
+    path('CharForm', views.CharForm, name = 'CharForm')
 ]
--- Phase 2 - Django Core/2.4 Views Templates Forms/djangomodels/genshin/views.py
+++ Phase 2 - Django Core/2.4 Views Templates Forms/djangomodels/genshin/views.py
@@ -2,8 +2,47 @@ from django.shortcuts import render, redirect
 from genshin.models import CharInfo
 from django.core.paginator import Paginator
 from .forms import CharInfoForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
 def home(request):
+    return render(request, 'genshin/home.html')
+
+
+
+def signup(request):
+    
+    if request.method == 'POST':
+        
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('login')
+    
+    else:
+        form = UserCreationForm()
+                
+    return render(request, 'genshin/signup.html', {'form':form})
+
+def login_view(request):
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    
+    else:
+        form = AuthenticationForm()
+            
+    
+    return render(request, 'genshin/login.html', {'form' : form})
+
+def listpage(request):
     chars = CharInfo.objects.all()
 
     paginator = Paginator(chars, 4)
@@ -12,19 +51,20 @@ def home(request):
 
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'genshin/index.html', {
+    return render(request, 'genshin/listpage.html', {
         'page_obj': page_obj
     })
     
-def charform(request):
+def CharForm(request):
     
     if request.method == 'POST':
-        form = charform(request.POST, request.FILES)
+        form = CharInfoForm(request.POST, request.FILES)
         
         if form.is_valid():
             form.save()
-            return redirect('charform')
+            return redirect('CharForm')
     
-    form = CharInfoForm()
+    else:
+        form = CharInfoForm()
     
     return render(request, 'genshin/charform.html', {'form': form})    
